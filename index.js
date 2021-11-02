a22288044d656d43a99c0366639c4748a43174f3
Jah821 <91166820+Jah821@users.noreply.github.com>
2021-11-02 14:30:12 +0000

    Finished code challenge

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
 //1
-function areBothSame(){
-  
+function areBothSame(x, y) {
+    // if two arguements are the same return true
+    // return x == y
+    if (x == y) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 //2
-function areBothEqual(){
-  
+function areBothEqual(x, y) {
+    // return x == y
+    if (x === y) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 //3
-function areAllFourEqual(){
-  
+function areAllFourEqual(x, y, z, k) {
+    if (x === y && x === z && x === k) {
+        return true
+    }
+    else if (y === z && y === k) {
+        return true
+    }
+    else if (z === k) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 
 //4
-function areAllFourSame(){
-  
+function areAllFourSame(x, y, z, k) {
+    if (x == y && x == z && x == k) {
+        return true
+    }
+    else if (y == z && y == k) {
+        return true
+    }
+    else if (z == k) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 //5
-function areAllFourSameOrEqual(){
-  
+function areAllFourSameOrEqual(x, y, z, k) {
+ if (x===y && y===z && z===k){
+     return "equal";
+ }else if ( x==y && y==z && z==k){
+     return "same"
+ }else{
+     return "null"
+ }
 }
 
-
+console.log(areAllFourSameOrEqual(true, false, 1, "1"))
